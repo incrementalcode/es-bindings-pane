b729700cdd161ce0e5542082d68589d84c9535aa
Bubblyworld <guy.paterson.jones@gmail.com>
2014-12-03 14:48:58 +0200

    Formatting for tree view.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-It can be difficult to establish a Javascript file's contents, and even more tricky to track down which other files it depends on. The aim is to provide this contextual information in a easy-to-use atom plugin.
+# ES Binding Pane
+
+It can be tricky to establish a Javascript file's contents, and even more difficult to track down which parts of other modules it depends on. The aim of this Atom plugin is to provide this contextual information as a simple pane on the right of the workspace.
 
 Currently this project is in an alpha stage, and not considered usable.
 
--- build/tree-component.js
+++ build/tree-component.js
@@ -18,17 +18,37 @@ var TreeComponent = React.createClass({
       child.key = index;
       return TreeComponent(child);
     }));
-    var h5Class = "es-binding ";
-    if (this.state.collapsed)
-      h5Class += "es-toggle-closed";
-    else
-      h5Class += "es-toggle-open";
-    return Reactionary.div(null, Reactionary.h5({
-      className: h5Class,
-      onClick: this.collapse
-    }, this.props.model.name), this.state.collapsed ? null : Reactionary.ul(null, children));
+    var iconClass = getIconClass.bind(this)();
+    var arrowClass = getArrowClass.bind(this)();
+    return Reactionary.div(null, Reactionary.div({
+      className: "es-container",
+      onClick: this.toggle
+    }, Reactionary.div({className: arrowClass}), Reactionary.h5({className: iconClass}, " " + this.props.model.name)), this.state.collapsed ? null : Reactionary.ul(null, children));
+    function getIconClass() {
+      switch (this.props.model.type) {
+        case "ImportDeclaration":
+          return "icon-cloud-download es-binding";
+        case "FunctionDeclaration":
+          return "icon-gear es-binding";
+        case "ClassDeclaration":
+          return "icon-puzzle-piece es-binding";
+        case "MethodDefinition":
+          return "icon-gear es-binding";
+      }
+      return "es-binding";
+    }
+    function getArrowClass() {
+      if (this.props.model.children.length > 0) {
+        if (this.state.collapsed)
+          return "es-icon icon-chevron-right";
+        else
+          return "es-icon icon-chevron-down";
+      } else {
+        return "";
+      }
+    }
   },
-  collapse: function() {
+  toggle: function() {
     this.setState({collapsed: !this.state.collapsed});
   }
 });
