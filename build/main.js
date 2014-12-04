86c3f9f8bb59f24414c8dbacc164cc75306f0fe5
Bubblyworld <guy.paterson.jones@gmail.com>
2014-12-04 12:17:07 +0200

    Move off React and into raw javascript. React is too unstable in atom at the moment.

--- build/main.js
+++ build/main.js
@@ -6,36 +6,38 @@ Object.defineProperties(exports, {
   __esModule: {value: true}
 });
 var $__cache__,
-    $__tree_45_component__,
-    $__tree_45_model__,
-    $__atom__;
+    $__tree_45_model__;
 var parseURI = ($__cache__ = require("./cache"), $__cache__ && $__cache__.__esModule && $__cache__ || {default: $__cache__}).parseURI;
-var TreeComponent = ($__tree_45_component__ = require("./tree-component"), $__tree_45_component__ && $__tree_45_component__.__esModule && $__tree_45_component__ || {default: $__tree_45_component__}).TreeComponent;
-var $__2 = ($__tree_45_model__ = require("./tree-model"), $__tree_45_model__ && $__tree_45_model__.__esModule && $__tree_45_model__ || {default: $__tree_45_model__}),
-    TreeModel = $__2.TreeModel,
-    syntaxTreeToModel = $__2.syntaxTreeToModel;
-var _atom = ($__atom__ = require("atom"), $__atom__ && $__atom__.__esModule && $__atom__ || {default: $__atom__}).default;
-var React = _atom.React;
-var treePanel;
+var $__1 = ($__tree_45_model__ = require("./tree-model"), $__tree_45_model__ && $__tree_45_model__.__esModule && $__tree_45_model__ || {default: $__tree_45_model__}),
+    TreeModel = $__1.TreeModel,
+    syntaxTreeToModel = $__1.syntaxTreeToModel;
+var treePanel,
+    treeComponent = null;
 function activate(state) {
   var treePanel = document.createElement('div');
   treePanel.className = 'es-bindings-pane';
   atom.workspace.addRightPanel({item: treePanel});
+  var unmountTreePanel = (function() {
+    if (treeComponent) {
+      treePanel.removeChild(treeComponent);
+      treeComponent = null;
+    }
+  });
   atom.workspace.observeActivePaneItem((function(item) {
     if (!item)
       return;
     if (item.getPath && item.getGrammar && item.getGrammar().name == 'JavaScript') {
       parseURI(item.getPath(), (function(error, result) {
         if (error) {
-          console.warn("Warning during parseURI() from main.js: " + error);
-          return React.unmountComponentAtNode(treePanel);
+          console.warn("Warning during parseURI() from main.js: " + error.stack);
+          return unmountTreePanel();
         }
-        var treeModel = result.treeModel;
-        var treeComponent = TreeComponent(treeModel.bake());
-        React.renderComponent(treeComponent, treePanel);
+        unmountTreePanel();
+        treeComponent = result.treeModel.render();
+        treePanel.appendChild(treeComponent);
       }));
     } else {
-      React.unmountComponentAtNode(treePanel);
+      unmountTreePanel();
     }
   }));
 }
--- build/tree-model.js
+++ build/tree-model.js
@@ -11,21 +11,76 @@ var TreeModel = function TreeModel(name, location, type, meta) {
   this.type = type;
   this.meta = meta;
   this.children = [];
+  this.collapsed = false;
 };
 ($traceurRuntime.createClass)(TreeModel, {
   addChild: function(child) {
     this.children.push(child);
     return this;
   },
-  bake: function() {
-    return {model: {
-        name: this.name,
-        location: this.location,
-        type: this.type,
-        meta: this.meta,
-        children: this.children.map((function(child) {
-          return child.bake();
-        }))
-      }};
+  render: function() {
+    var $__0 = this;
+    var root = document.createElement('div');
+    var container = document.createElement('div');
+    container.className = 'es-container';
+    var arrow = document.createElement('div');
+    arrow.className = this.getArrowClass();
+    arrow.onclick = (function() {
+      return $__0.toggleCollapsed(childList);
+    });
+    var text = document.createElement('h5');
+    text.className = this.getIconClass();
+    text.textContent = " " + this.name;
+    text.onclick = (function() {
+      return $__0.highlightLocation();
+    });
+    var childList = document.createElement('ul');
+    childList.className = 'es-ul';
+    this.children.map((function(child) {
+      return childList.appendChild(child.render());
+    }));
+    if (this.collapsed)
+      childList.style.setProperty('display', 'none');
+    container.appendChild(arrow);
+    container.appendChild(text);
+    root.appendChild(container);
+    root.appendChild(childList);
+    return root;
+  },
+  getIconClass: function() {
+    switch (this.type) {
+      case "ImportDeclaration":
+        return "icon-cloud-download es-binding";
+      case "ExportDeclaration":
+        return "icon-cloud-upload es-binding";
+      case "FunctionDeclaration":
+        return "icon-gear es-binding";
+      case "ClassDeclaration":
+        return "icon-puzzle es-binding";
+      case "MethodDefinition":
+        return "icon-gear es-binding";
+    }
+    return "es-binding";
+  },
+  getArrowClass: function() {
+    if (this.children.length > 0) {
+      return "es-icon icon-chevron-down";
+    } else {
+      return "";
+    }
+  },
+  toggleCollapsed: function(childList) {
+    this.collapsed = !this.collapsed;
+    if (this.collapsed)
+      childList.style.setProperty('display', 'none');
+    else
+      childList.style.removeProperty('display');
+  },
+  highlightLocation: function() {
+    var location = this.location;
+    var range = [[location.start.line - 1, location.start.column], [location.end.line - 1, location.end.column]];
+    var editor = atom.workspace.getActiveTextEditor();
+    editor.setSelectedBufferRange(range);
+    editor.scrollToBufferPosition(range[0]);
   }
 }, {});
